d9f0db61590be301e914866e4622b14f3d0e87d0
Deevesh Beegun <deeveshbeegun@gmail.com>
2021-09-02 15:14:00 +0200

    Renamed a bunch of files.

--- neural_machine_translation/translation/bleu_score.sh
+++
@@ -1,4 +0,0 @@
-fairseq-interactive --input=source_english.txt data-bin-en2xh/baseline-tokenized.en-xh --path checkpoints/checkpoint_best.pt --batch-size 1 --beam 5 > target.txt
-
-grep ^H ./target.txt | cut -f3- > ./target_mod.txt
-fairseq-score --sys ./target_mod.txt --ref ./target_xhosa.txt
