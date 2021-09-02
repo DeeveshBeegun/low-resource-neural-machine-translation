727bdf6e16dff53a650c58072949cad1fc3b06ef
Deevesh Beegun <deeveshbeegun@gmail.com>
2021-09-02 13:31:25 +0200

    Modified directory path.

--- bleu_score.sh
+++ bleu_score.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
 
-src='en'
-tgt='xh'
+src='English'
+tgt='IsiXhosa'
 
-file_path='autshumato_eval_set'
+file_path='autshumato_eval_set/'
 dir_path='neural_machine_translation/translation/'
 
-cat ./$file_path *.$src.*.txt > combined_autshumato_$src.txt
-cat ./$file_path *.$tgt.*.txt > combined_autshumato_$tgt.txt
+cd $file_path
+
+cat  Autshumato.EvaluationSet.$src.*.txt >> combined_autshumato_$src.txt
+cat  Autshumato.EvaluationSet.$tgt.*.txt >> combined_autshumato_$tgt.txt
+
+cd ../
 
 fairseq-interactive --input=./combined_autshumato_$src.txt $dir_path/data-bin-en2xh/baseline-tokenized.en-xh --path checkpoints/checkpoint_best.pt --batch-size 1 --beam 5 > translation.txt
 
