9ffdcb6014cee486ce2c738d79739f84f38eec24
Deevesh Beegun <deeveshbeegun@gmail.com>
2021-09-02 13:17:20 +0200

    Added fairseq score to evaluate trained model.

--- bleu_score.sh
+++ bleu_score.sh
@@ -1,4 +1,14 @@
-fairseq-interactive --input=source_english.txt data-bin-en2xh/baseline-tokenized.en-xh --path checkpoints/checkpoint_best.pt --batch-size 1 --beam 5 > target.txt
 
-grep ^H ./target.txt | cut -f3- > ./target_mod.txt
-fairseq-score --sys ./target_mod.txt --ref ./target_xhosa.txt
+src='en'
+tgt='xh'
+
+file_path='autshumato_eval_set'
+dir_path='neural_machine_translation/translation/'
+
+cat ./$file_path *.$src.*.txt > combined_autshumato_$src.txt
+cat ./$file_path *.$tgt.*.txt > combined_autshumato_$tgt.txt
+
+fairseq-interactive --input=./combined_autshumato_$src.txt $dir_path/data-bin-en2xh/baseline-tokenized.en-xh --path checkpoints/checkpoint_best.pt --batch-size 1 --beam 5 > translation.txt
+
+grep ^H ./translation.txt | cut -f3- > ./translation_mod.txt
+fairseq-score --sys ./translation_mod.txt --ref ./combined_autshumato_$tgt.txt
